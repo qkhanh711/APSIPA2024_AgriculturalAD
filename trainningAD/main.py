aef69830bc9f56571d0148ccc509261c9dead896
qkhanh711 <quockhanh07112003@gmail.com>
2024-07-18 22:15:24 +0700

    save results, add scripts example

--- trainningAD/main.py
+++ trainningAD/main.py
@@ -86,49 +86,15 @@ engine = Engine(
 
 engine.fit(datamodule=datamodule, model=model)
 
-engine.test(datamodule=datamodule, model=model)
+results = engine.test(datamodule=datamodule, model=model)
 
-inference_dataset = PredictDataset(path=args.root + f"/{args.cls}/test/tear/{args.test_id}.png")
-inference_dataloader = DataLoader(dataset=inference_dataset, num_workers=47)
 
-predictions = engine.predict(model=model, dataloaders=inference_dataloader)[0]
 
-print(
-    f'Image Shape: {predictions["image"].shape},\n'
-    f'Anomaly Map Shape: {predictions["anomaly_maps"].shape}, \n'
-    f'Predicted Mask Shape: {predictions["pred_masks"].shape}',
-)
+import os
+os.makedirs("results_metrics", exist_ok=True)
 
-image_path = predictions["image_path"][0]
-image_size = predictions["image"].shape[-2:]
-image = np.array(Image.open(image_path).resize(image_size))
+results = results[0]
 
-plt.imshow(image)
-plt.savefig(f"{args.cls}_image.png")
-plt.show()
-import cv2
-
-cv2.imwrite(f"{args.cls}_predict.png", image)
-
-anomaly_map = predictions["anomaly_maps"][0]
-anomaly_map = anomaly_map.cpu().numpy().squeeze()
-plt.savefig(f"{args.cls}_anomaly_map.png")
-plt.imshow(anomaly_map)
-plt.savefig(f"{args.cls}_anomaly_map_1.png")
-# plt.show()
-
-heat_map = superimpose_anomaly_map(anomaly_map=anomaly_map, image=image, normalize=True)
-plt.savefig(f"{args.cls}_heat_map.png")
-plt.imshow(heat_map)
-plt.savefig(f"{args.cls}_heat_map_1.png")
-# plt.show()
-
-pred_score = predictions["pred_scores"][0]
-pred_labels = predictions["pred_labels"][0]
-print(pred_score, pred_labels)
-
-pred_masks = predictions["pred_masks"][0].squeeze().cpu().numpy()
-plt.savefig(f"{args.cls}_pred_masks.png")
-plt.imshow(pred_masks)
-plt.savefig(f"{args.cls}_pred_masks_1.png")
-# plt.show()
+import json
+with open(f"results/{args.cls}_{args.model}.json", "w") as f:
+    json.dump(results, f)
